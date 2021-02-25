102a9ffa51de771cba89e30a092b6d114f161db8
Arghun-dev <arghun.developer@gmail.com>
2021-02-25 18:01:22 +0330

    created and implemented schema.js file

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,9 +5,14 @@
 // in this app I have books and authors, so ultimately we need to describe these two object types in the
 // schema file and the relations between them and also how to query these object types to get data
 
+// What we've done here?
+// first we inported graphql and then we grabbed these things from GraphQL
+// then what we've done is we defined our first object type and that is a BookType
+// then we defined the RootQuery which defined how we initially jump into the graph
+
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -17,3 +22,23 @@ const BookType = new GraphQLObjectType({
     genre: { type: GraphQLString }
   })
 });
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to get data from db or other source
+        // parent will come into play when we want relationships between data
+      }
+    }
+  }
+})
+
+// here we're going to define graphQL schema
+// inside here we're gonna pass through our initial root query
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
