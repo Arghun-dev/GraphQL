f76264e42a6f9576804831f5fdc5c001b47b9bcf
Arghun-dev <arghun.developer@gmail.com>
2021-02-26 21:21:32 +0330

    implented relations in graphql get author of books

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -17,9 +17,9 @@ const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt }
 
 // dummy data
 var books = [
-  {name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
-  {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
-  {name: 'The Long Earth', genre: 'Sci-Fi', id: '3'}
+  {name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'},
+  {name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
+  {name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'}
 ]
 
 var authors = [
@@ -33,12 +33,18 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args){
+        return _.find(authors, { id: parent.authorId })
+      }
+    }
   })
 });
 
 const AuthorType = new GraphQLObjectType({
-  name: 'Authoe',
+  name: 'Author',
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
